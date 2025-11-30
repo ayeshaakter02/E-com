9a37b092f1cd7747e36f5194a3dcfd32f1f0f282
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-11-30 23:08:46 +0600

    shop page design

--- e-com_frontend/src/components/common/Product.jsx
+++ e-com_frontend/src/components/common/Product.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
+"use client";
 
-const Product = ({product}) => {
+import Link from "next/link";
+import React from "react";
+
+const Product = ({ product }) => {
   return (
     <div>
-        <div className="shadow-sm bg-white p-1.5 rounded-md overflow-hidden cursor-pointer relative hover:shadow-md">
+      <div className="shadow-sm bg-white p-1.5 rounded-md overflow-hidden cursor-pointer relative hover:shadow-md">
         <a href="javascript:void(0);" className="block">
-          <div className="aspect-square">
-            <img
-              src={product.image[0]}
-              className="w-full h-full object-cover object-top transform-gpu transition-transform duration-400 ease-in-out hover:scale-150"
-            />
-          </div>
-          <div 
-          key={product._id}
-          className="p-3 pb-1.5 ">
-            <div className='flex justify-between'>
-                <h6 className="text-slate-900 text-lg font-bold truncate">
-              {product.title}
-            </h6>
-            <h6 className="text-gray-400 text-sm font-medium truncate">
-              {product.price} BDT
-            </h6>
+          <Link href={`/shop/${product.slug}`}>
+            <div className="aspect-square">
+              <img
+                src={product.image[0]}
+                className="w-full h-full object-cover object-top transform-gpu transition-transform duration-400 ease-in-out hover:scale-150"
+              />
             </div>
-            
+          </Link>
+
+          <div key={product._id} className="p-3 pb-1.5 ">
+            <div className="flex justify-between">
+              <h6 className="text-slate-900 text-lg font-bold truncate">
+                {product.title}
+              </h6>
+              <h6 className="text-gray-400 text-sm font-medium truncate">
+                {product.price} BDT
+              </h6>
+            </div>
+
             <h6 className="text-slate-900 text-sm font-medium truncate">
               {product.description}
             </h6>
-            
           </div>
         </a>
       </div>
-</div>
-  )
-}
+    </div>
+  );
+};
 
-export default Product
+export default Product;
