e606e578dda0d389c59011c490fac19431e31fb4
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2026-05-21 19:45:34 +0600

    react-router set up

--- e-com_dashboard/src/App.jsx
+++ e-com_dashboard/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <><RouterProvider router={router} /></>
   )
 }
 
