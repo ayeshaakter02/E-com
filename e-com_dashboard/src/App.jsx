4e504d6473987506855f839147e7c899d6c9bd27
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2026-05-21 20:47:56 +0600

    design signup & login page

--- e-com_dashboard/src/App.jsx
+++ e-com_dashboard/src/App.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/signup",
+    element: <Signup/>,
   },
 ]);
 
